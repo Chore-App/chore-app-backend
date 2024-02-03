a660a647c92e88bc9ea141e611760962d3b83bbd
Dhyan Patel <dhyanpp@gmail.com>
2024-02-02 21:58:44 -0600

    Boilerplace and mimic code

---
+++ src/test/kotlin/app/chores/choreappbackend/ChoreAppBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package app.chores.choreappbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChoreAppBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
